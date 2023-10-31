300d918ff03e768cf8da05df8df07eb1f721fce1
Draminhon <britomurilorog@gmail.com>
2023-10-31 17:47:06 -0300

    Estilizando a tela de login

--- assets/css/Css.js
+++ assets/css/Css.js
@@ -21,6 +21,40 @@ const css = StyleSheet.create({
     buton_home:{
       width: 'auto',
       height: 'auto'
+    },
+    darkbg:{
+      backgroundColor: "#333" 
+    },
+    login__logomarca:{
+      marginBottom: 10
+    },
+    login__msg:(text = 'none')=>({
+      fontWeight: "bold",
+      fontSize: 22,
+      color: "red",
+      marginTop: 5,
+      marginBottom: 15,
+      display: text
+    }),
+    login__form:{
+      width: "80%"
+    },
+    login__input:{
+      backgroundColor: "#fff",
+      fontSize: 19,
+      padding: 7,
+      marginBottom: 15
+    },
+    login__buton:{
+      padding: 12,
+      backgroundColor: "#F58634",
+      alignSelf: "center",
+      borderRadius: 5
+    },
+    login__buttonText:{
+      fontWeight: "bold",
+      fontSize: 21,
+      color: "#333"
     }
   });
 
--- view/Login.js
+++ view/Login.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Platform} from 'react-native'
 import { css } from '../assets/css/Css';
 
 export default function LoginScreen(props)
 {
+
+    const [display, setDisplay] = useState('none');
     return(
         <KeyboardAvoidingView  behavior = {Platform.OS == "ios" ? "padding" : "height"}style = {[css.container, css.darkbg]}>
-            <View>
+            <View style = {css.login__logomarca}>
             <Image source = {require('../assets/img/a.png/')}/>
             </View>
 
             <View>
-                <Text style = {css.login__msg}>Usuário ou senha inválidos!</Text>
+                <Text style = {css.login__msg(display)}>Usuário ou senha inválidos!</Text>
             </View>
 
             <View style = {css.login__form}>
