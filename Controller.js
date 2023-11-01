3a961b0e0f48387ebc9ed51b72c71d2be2b8de38
Draminhon <britomurilorog@gmail.com>
2023-11-01 17:15:30 -0300

    criando um crud para o banco de dados no entry point utilizando express e sequelize

--- Controller.js
+++ Controller.js
@@ -1,14 +1,54 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const models = require('./models');
  
 const app = express();
 app.use(cors());
  
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/',(req,res)=>{
-        res.send("Meu servidr back end já está rodado");
+let user = models.User;
+let tracking = models.Tracking;
+let product = models.Product;
+
+app.get('/create',async (req,res)=>{
+
+        let create = await user.create({
+        firstName: "Joao",
+        password: "abc",
+        createdAt: new Date(),
+        updatedAt: new Date()        
+        });
+        res.send('Usuário criado com sucesso');
+        
+});
+
+app.get('/read', async (req,res)=>{
+
+   let read = await user.findAll({
+        raw: true
+   });
+   console.log(read);
+
+});
+
+app.get('/update', async (req,res)=>{
+
+        let update = await user.findByPk(2, 
+                {include:[{all:true}]}
+                ).then((response)=>{
+              console.log(response);
+        })
+
+})
+
+app.get('/delete', async (req,res)=>{
+        user.destroy({
+                where: {id:1}
+        });
+
+
 })
 
 let port = process.env.PORT || 3000;
